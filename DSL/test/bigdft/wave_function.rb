451f367b035b1003605477f201a4cc4dc35653ef
Brice Videau <brice.videau@imag.fr>
2017-09-11 18:31:55 +0200

    Tests for inverse :grow direction.

--- DSL/test/bigdft/wave_function.rb
+++ DSL/test/bigdft/wave_function.rb
@@ -1,6 +1,8 @@
 class TestWaveFunction < Minitest::Test
   def setup
     @s = System::new([42, 28, 24], [BC::Free, BC::Per, BC::Free], :s1)
+
+    @c = System::new([86, 28, 40], [BC::Free, BC::Per, BC::Free], :s0, transitions:  OrderedTransitions::new(:s1, :s0, :r, :shrink))
   end
 
   def test_new
@@ -25,6 +27,7 @@ class TestWaveFunction < Minitest::Test
   end
 
   def test_to_s1s0
+    5.times do |i|
     w = WaveFunction::new(@s, random: true)
     w2 = w.to([:s0, :s1, :s1])
     refute_equal( w.data_space.data, w2.data_space.data )
@@ -51,7 +54,41 @@ class TestWaveFunction < Minitest::Test
     w4p = w3p.to([:s0, :s0, :s0])
     assert( (w2.restricted_data - w4p.restricted_data).abs.max < 10e-16 )
     w3 = w2.to([:s1, :s1, :s1])
+    assert( (w.restricted_data - w3.restricted_data).abs.max < 10e-15 )
+    end
+  end
+
+  def test_to_s0s1
+    5.times do |i|
+    w = WaveFunction::new(@c, random: true)
+
+    w2 = w.to([:s1, :s0, :s0])
+    refute_equal( w.data_space.data, w2.data_space.data )
+    assert_equal( w2.shape.to_a, w2.data_space.data.shape )
+    w3 = w2.to([:s0, :s0, :s0])
     assert( (w.restricted_data - w3.restricted_data).abs.max < 10e-16 )
+
+    w2 = w.to([:s0, :s1, :s0])
+    refute_equal( w.data_space.data, w2.data_space.data )
+    assert_equal( w2.shape.to_a, w2.data_space.data.shape )
+    w3 = w2.to([:s0, :s0, :s0])
+    assert( (w.restricted_data - w3.restricted_data).abs.max < 10e-16 )
+
+
+    w2 = w.to([:s0, :s0, :s1])
+    refute_equal( w.data_space.data, w2.data_space.data )
+    assert_equal( w2.shape.to_a, w2.data_space.data.shape )
+    w3 = w2.to([:s0, :s0, :s0])
+    assert( (w.restricted_data - w3.restricted_data).abs.max < 10e-16 )
+
+    w2 = w.to([:s1, :s1, :s1])
+    w2p = w.to([:s1, :s0, :s0])
+    w3p = w2p.to([:s1, :s1, :s0])
+    w4p = w3p.to([:s1, :s1, :s1])
+    assert( (w2.restricted_data - w4p.restricted_data).abs.max < 10e-16 )
+    w3 = w2.to([:s0, :s0, :s0])
+    assert( (w.restricted_data - w3.restricted_data).abs.max < 10e-15 )
+    end
   end
 
 end
