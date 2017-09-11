7ba1816b0a4bbe150a201927582213198e2e4095
Brice Videau <brice.videau@imag.fr>
2017-09-11 12:38:49 +0200

    Added test for reversed grow direction system.

--- DSL/test/bigdft/system.rb
+++ DSL/test/bigdft/system.rb
@@ -4,23 +4,25 @@ class TestSystem < Minitest::Test
     @s1 = System::new([42, 28, 24], [BC::Free]*3, :s1)
     @s2 = System::new([42, 28, 24], [BC::Free, BC::Per, BC::Free], :s0)
     @s3 = System::new([99, 56, 61], [BC::Free, BC::Per, BC::Free], :r)
+    @s4 = System::new([100, 28, 54], [BC::Free, BC::Per, BC::Free], :s1, transitions:  OrderedTransitions::new(:s1, :s0, :r, :shrink))
   end
 
   def test_system_new
-    s = System::new([42, 28, 24], [BC::Free]*3, :s1)
-    assert_equal([42, 28, 24], s.reference_dimensions)
-    assert_equal([BC::Free, BC::Free, BC::Free], s.boundary_conditions)
-    assert_equal(3, s.dimension)
+    assert_equal([42, 28, 24], @s1.reference_dimensions)
+    assert_equal([BC::Free, BC::Free, BC::Free], @s1.boundary_conditions)
+    assert_equal(3, @s1.dimension)
 
-    s = System::new([42, 28, 24], [BC::Free, BC::Per, BC::Free], :s0)
-    assert_equal([42, 28, 24], s.reference_dimensions)
-    assert_equal([BC::Free, BC::Per, BC::Free], s.boundary_conditions)
-    assert_equal(3, s.dimension)
+    assert_equal([42, 28, 24], @s2.reference_dimensions)
+    assert_equal([BC::Free, BC::Per, BC::Free], @s2.boundary_conditions)
+    assert_equal(3, @s3.dimension)
 
-    s = System::new([99, 56, 61], [BC::Free, BC::Per, BC::Free], :r)
-    assert_equal([99, 56, 61], s.reference_dimensions)
-    assert_equal([BC::Free, BC::Per, BC::Free], s.boundary_conditions)
-    assert_equal(3, s.dimension)
+    assert_equal([99, 56, 61], @s3.reference_dimensions)
+    assert_equal([BC::Free, BC::Per, BC::Free], @s3.boundary_conditions)
+    assert_equal(3, @s3.dimension)
+
+    assert_equal([100, 28, 54], @s4.reference_dimensions)
+    assert_equal([BC::Free, BC::Per, BC::Free], @s4.boundary_conditions)
+    assert_equal(3, @s4.dimension)
   end
 
   def test_dimensions
@@ -37,6 +39,11 @@ class TestSystem < Minitest::Test
     assert_equal([99, 56, 61], @s3.dimensions(:r))
     assert_equal([99-15, 56, 61-15], @s3.dimensions(:s0))
     #assert_equal([(99-15)/2, 56/2, (61-15)/2], @s3.dimensions(:s1))
+
+    assert_equal([100, 28, 54], @s4.dimensions(:s1))
+    assert_equal([100-14, 28, 54 - 14], @s4.dimensions(:s0))
+    assert_equal([100-30, 28, 54 - 30], @s4.dimensions(:r))
+    assert_equal([100, 28, 54 - 30], @s4.dimensions([:s1, :s0, :r]))
   end
 
   def test_shapes
@@ -49,6 +56,11 @@ class TestSystem < Minitest::Test
     assert_equal([21 - 7, 2, 14, 2, 12 - 7 + 1, 2], @s2.shapes(:s1))
     assert_equal([42 + 15 + 3, 28, 24 + 15 + 1], @s2.shapes(:r))
     assert_equal([21 - 7, 2, 2, 14, 24 + 15 + 1], @s2.shapes([:s1, :s0, :r]))
+
+    assert_equal([50, 2, 14, 2, 27 + 1, 2], @s4.shapes(:s1))
+    assert_equal([2, 50 - 7 + 1, 2, 14, 2, 27 - 7], @s4.shapes(:s0))
+    assert_equal([100 - 30 + 2, 28, 54 - 30], @s4.shapes(:r))
+    assert_equal([50, 2, 2, 14, 54 - 30], @s4.shapes([:s1, :s0, :r]))
   end
 
   def test_leading_dimensions
@@ -61,6 +73,11 @@ class TestSystem < Minitest::Test
     assert_equal([42 - 14, 28, 24 - 14 + 2], @s2.leading_dimensions(:s1))
     assert_equal([42 + 15 + 3, 28, 24 + 15 + 1], @s2.leading_dimensions(:r))
     assert_equal([42 - 14, 28, 24 + 15 + 1], @s2.leading_dimensions([:s1, :s0, :r]))
+
+    assert_equal([100, 28, 54 + 2], @s4.leading_dimensions(:s1))
+    assert_equal([100 - 14 + 2, 28, 54 - 14], @s4.leading_dimensions(:s0))
+    assert_equal([100 - 30 + 2, 28, 54 - 30], @s4.leading_dimensions(:r))
+    assert_equal([100, 28, 54 - 30], @s4.leading_dimensions([:s1, :s0, :r]))
   end
 
 end
